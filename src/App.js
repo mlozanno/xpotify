987305b592bf4be4f05febe8e0fa4e79b9534dd6
Marcus Lozano <contato@lozanno.com.br>
2020-04-30 22:07:19 -0300

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from '~/store';
 
 const App = () => {
 	return (
-		<>
+		<Provider store={store}>
 			<h1>Hello XPotify</h1>
-		</>
+		</Provider>
 	);
 };
 
