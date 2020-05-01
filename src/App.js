f1afb28a214684c729af0ddf6f07f2f88c160ead
Marcus Lozano <contato@lozanno.com.br>
2020-04-30 23:19:27 -0300

    Create route system with private routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import history from '~/services/history';
 
 import store from '~/store';
+
+import Routes from '~/routes';
 
 const App = () => {
 	return (
 		<Provider store={store}>
-			<h1>Hello XPotify</h1>
+			<Router history={history}>
+				<Routes />
+			</Router>
 		</Provider>
 	);
 };
