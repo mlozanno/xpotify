2e25901fd6bd347381101eedc978b4bd05f439e5
Marcus Lozano <contato@lozanno.com.br>
2020-05-02 19:40:45 -0300

    Spotify api config

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = '888787c49ec8465dadeed91ed321990d';
+export const clientSecret = '29cf9524e1594384b8ea5fc4ab711d6e';
+export const redirectUri = 'http://localhost:3000';
+export const scopes = [
+	'user-top-read',
+	'user-read-currently-playing',
+	'user-read-playback-state',
+];
