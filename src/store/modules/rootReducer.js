75bb8851c7aee9caf1378a370f7d4c2ade81ab02
Marcus Lozano <contato@lozanno.com.br>
2020-05-02 05:26:16 -0300

    Add error reducer to the rootReducer

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import auth from './auth/reducer';
 import albums from './albums/reducer';
+import error from './error/reducer';
 
 export default combineReducers({
 	auth,
 	albums,
+	error,
 });
