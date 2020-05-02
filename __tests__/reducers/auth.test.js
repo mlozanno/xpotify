ad532f7a01f269b9a185e1cca595eeb65770dce4
Marcus Lozano <contato@lozanno.com.br>
2020-05-02 04:58:40 -0300

    Create reducer for authentication

---
+++ __tests__/reducers/auth.test.js
@@ -0,0 +1,39 @@
+import reducer, { INITIAL_STATE } from '~/store/modules/auth/reducer';
+import { SET_AUTH_DATA } from '~/store/modules/auth/actions';
+
+const newState = {
+	access_token: 'd41d8cd98f00b204e9800998ecf8427e',
+	expires_in: 3600,
+	refresh_token: 'd41d8cd98f00b204e9800998ecf8427e',
+	scope: 'user scop',
+	token_type: 'Bearer',
+	loading: false,
+	signed: true,
+};
+
+describe('Auth reducers', () => {
+	it('Should return initial state when type is undefined', () => {
+		expect(reducer(undefined, {})).toEqual(INITIAL_STATE);
+	});
+
+	it('Should return initial state if action does not exists', () => {
+		expect(reducer(INITIAL_STATE, { type: 'UNKNOWN_TYPE' })).toEqual(
+			INITIAL_STATE
+		);
+	});
+
+	it('Should handle auth data', () => {
+		expect(
+			reducer(INITIAL_STATE, {
+				type: SET_AUTH_DATA,
+				payload: { ...newState },
+			})
+		).toEqual(newState);
+	});
+
+	it('Should action name and value to be equals', () => {
+		console.log(SET_AUTH_DATA);
+		console.log(String(SET_AUTH_DATA));
+		expect(SET_AUTH_DATA).toEqual('SET_AUTH_DATA');
+	});
+});
--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,3 +1,26 @@
-export default function auth() {
-	return {};
+import { SET_AUTH_DATA } from './actions';
+
+export const INITIAL_STATE = {
+	access_token: '',
+	expires_in: 0,
+	refresh_token: '',
+	scope: '',
+	token_type: '',
+	loading: true,
+	signed: false,
+};
+
+export default function auth(state = INITIAL_STATE, action) {
+	switch (action.type) {
+		case SET_AUTH_DATA:
+			return {
+				...state,
+				...action.payload,
+				loading: false,
+				signed: true,
+			};
+
+		default:
+			return state;
+	}
 }
