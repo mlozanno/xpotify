5575c58e3c8013de980f7999fda2a65576ee35bf
Marcus Lozano <contato@lozanno.com.br>
2020-05-02 04:03:53 -0300

    Create instances for auth and api

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const auth = axios.create({
+export const authService = axios.create({
 	baseURL: 'https://accounts.spotify.com',
 });
 
-export const api = axios.create({
+export const apiService = axios.create({
 	baseURL: 'https://api.spotify.com/v1/',
 });
