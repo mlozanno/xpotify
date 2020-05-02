3422ca0bde8b9370dc46c51f706f93a752f5ef17
Marcus Lozano <contato@lozanno.com.br>
2020-05-02 04:02:58 -0300

    Fix prop-types for <Album />

--- src/components/Album/index.js
+++ src/components/Album/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import { StyledAlbum } from './styles';
 
@@ -15,5 +15,12 @@ const Album = ({ id, images, name, artists }) => (
 		<h2>{mergeArtists(artists)}</h2>
 	</StyledAlbum>
 );
+
+Album.propTypes = {
+	id: PropTypes.string.isRequired,
+	images: PropTypes.arrayOf(PropTypes.object).isRequired,
+	name: PropTypes.string.isRequired,
+	artists: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Album;
