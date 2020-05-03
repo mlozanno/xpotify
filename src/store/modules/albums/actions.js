207a8026b55e7989ddfa8591d5996b3bfa6a3de3
Marcus Lozano <contato@lozanno.com.br>
2020-05-03 20:55:26 -0300

    Create search reducer

--- src/store/modules/albums/actions.js
+++ src/store/modules/albums/actions.js
@@ -1,1 +1,2 @@
 export const SET_ALBUMS = 'SET_ALBUMS';
+export const SET_SEARCH = 'SET_SEARCH';
--- src/store/modules/albums/reducer.js
+++ src/store/modules/albums/reducer.js
@@ -1,11 +1,22 @@
-import { SET_ALBUMS } from './actions';
+import { SET_ALBUMS, SET_SEARCH } from './actions';
 
-export const INITIAL_STATE = [];
+export const INITIAL_STATE = {
+	result: [],
+	search: '',
+};
 
 export default function albums(state = INITIAL_STATE, action) {
 	switch (action.type) {
 		case SET_ALBUMS:
-			return action.payload;
+			return {
+				...state,
+				result: [...action.payload],
+			};
+		case SET_SEARCH:
+			return {
+				...state,
+				search: action.payload.search,
+			};
 
 		default:
 			return state;
