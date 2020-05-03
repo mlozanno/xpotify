e05b4153a523e012a909cb1fbadfe729e0736037
Marcus Lozano <contato@lozanno.com.br>
2020-05-03 05:39:44 -0300

    Refactory Dashboard page
    Now responsible for searching the albums, updating the state and rendering the <AlbumList />

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-// import { Container } from './styles';
+import { getAlbums } from '~/services/albums';
 
-const Dashboard = () => <h1>Dashboard</h1>;
+import store from '~/store';
+import { SET_ALBUMS } from '~/store/modules/albums/actions';
+
+import Layout from '~/components/Layout';
+import Input from '~/components/Input';
+import AlbumList from '~/components/AlbumList';
+import Album from '~/components/Album';
+
+const Dashboard = () => {
+	const [search, setSearch] = useState('');
+	const { auth, albums } = store.getState();
+
+	const dispatch = useDispatch();
+
+	const handleAlbums = async ({ target: { value } }) => {
+		setSearch(value);
+
+		if (value.length) {
+			try {
+				const payload = await getAlbums(auth.access_token, value, 10);
+
+				dispatch({ type: SET_ALBUMS, payload });
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	};
+
+	return (
+		<Layout>
+			<Input handleChange={handleAlbums} value={search} />
+
+			<AlbumList>
+				{albums.map(album => (
+					<Album {...album} key={album.id} />
+				))}
+			</AlbumList>
+		</Layout>
+	);
+};
 
 export default Dashboard;
