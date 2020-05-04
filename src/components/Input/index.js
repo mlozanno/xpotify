5de99e5cdd573bf1e3007e128780f4814b014355
Marcus Lozano <contato@lozanno.com.br>
2020-05-04 03:15:50 -0300

    Get input value from redux state

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
 import { StyledInput } from './styles';
 
-const Input = ({ value, handleChange }) => (
-	<StyledInput>
-		<label htmlFor="search">
-			Busque por artistas, álbums ou músicas
-			<input
-				type="text"
-				onChange={handleChange}
-				value={value}
-				id="search"
-				placeholder="Comece a escrever..."
-			/>
-		</label>
-	</StyledInput>
-);
+const Input = ({ handleChange }) => {
+	const { search } = useSelector(state => state.albums);
+
+	return (
+		<StyledInput>
+			<label htmlFor="search">
+				Busque por artistas, álbums ou músicas
+				<input
+					type="text"
+					onChange={handleChange}
+					value={search}
+					id="search"
+					placeholder="Comece a escrever..."
+				/>
+			</label>
+		</StyledInput>
+	);
+};
 
 Input.propTypes = {
-	value: PropTypes.string.isRequired,
 	handleChange: PropTypes.func.isRequired,
 };
 
