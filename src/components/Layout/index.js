c5adddf290e5e08f31d2a47f49e6e3097fc634f3
Marcus Lozano <contato@lozanno.com.br>
2020-05-04 01:49:18 -0300

    Add Player to the layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
 import { StyledLayout } from './styles';
 
-const Layout = ({ children }) => <StyledLayout>{children}</StyledLayout>;
+import Player from '~/components/Player';
+
+const Layout = ({ children }) => {
+	const {
+		currentTrack: { trackName },
+	} = useSelector(state => state.tracks);
+
+	return (
+		<StyledLayout>
+			{children}
+			{trackName && <Player />}
+		</StyledLayout>
+	);
+};
 
 Layout.propTypes = {
 	children: PropTypes.node.isRequired,
