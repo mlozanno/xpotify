3b4c77cc21c44c774de7a7c14ae2deaaa27fc3d9
Marcus Lozano <contato@lozanno.com.br>
2020-05-03 21:36:51 -0300

    Fix albums test to receive result array

--- __tests__/reducers/albums.test.js
+++ __tests__/reducers/albums.test.js
@@ -1,32 +1,35 @@
 import reducer, { INITIAL_STATE } from '~/store/modules/albums/reducer';
 import { SET_ALBUMS } from '~/store/modules/albums/actions';
 
-const newState = [
-	{
-		id: '3I9Z1nDCL4E0cP62flcbI5',
-		name: 'Appetite For Destruction',
-		artists: [{ name: "Guns N' Roses" }],
-		images: [
-			{
-				height: 300,
-				url: 'http://images.com/300',
-				width: 300,
-			},
-		],
-	},
-	{
-		id: '3I9Z1nDCL4E0cP62flcbI5',
-		name: 'Appetite For Destruction',
-		artists: [{ name: "Guns N' Roses" }],
-		images: [
-			{
-				height: 300,
-				url: 'http://images.com/300',
-				width: 300,
-			},
-		],
-	},
-];
+const payload = {
+	result: [
+		{
+			id: '3I9Z1nDCL4E0cP62flcbI5',
+			name: 'Appetite For Destruction',
+			artists: [{ name: "Guns N' Roses" }],
+			images: [
+				{
+					height: 300,
+					url: 'http://images.com/300',
+					width: 300,
+				},
+			],
+		},
+		{
+			id: '3I9Z1nDCL4E0cP62flcbI5',
+			name: 'Appetite For Destruction',
+			artists: [{ name: "Guns N' Roses" }],
+			images: [
+				{
+					height: 300,
+					url: 'http://images.com/300',
+					width: 300,
+				},
+			],
+		},
+	],
+	search: '',
+};
 
 describe('Albums reducer', () => {
 	it('Should return initial state when type is undefined', () => {
@@ -43,9 +46,9 @@ describe('Albums reducer', () => {
 		expect(
 			reducer(INITIAL_STATE, {
 				type: SET_ALBUMS,
-				payload: [...newState],
+				payload: payload.result,
 			})
-		).toEqual(newState);
+		).toEqual(payload);
 	});
 
 	it('Should action name and value to be equals', () => {
