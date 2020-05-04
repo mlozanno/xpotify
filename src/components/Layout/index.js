69628c6d90e2c952f0c130e7b176e35aaf131df0
Marcus Lozano <contato@lozanno.com.br>
2020-05-03 21:25:40 -0300

    Add prop-types

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { StyledLayout } from './styles';
 
 const Layout = ({ children }) => <StyledLayout>{children}</StyledLayout>;
+
+Layout.propTypes = {
+	children: PropTypes.node.isRequired,
+};
 
 export default Layout;
